fcfb14bd1ee2fbb5b7b22eee77f4386a09738e5e
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-06-07 07:23:07 +0800

    feature: logger middleware

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import store from "./store";
 import { actions } from "./ducks/freezer";
 import { VANILLA } from "./constants/flavors";
 
-store.subscribe(() => console.log(store.getState()));
-
 store.dispatch(actions.updateTemperature(-8));
 store.dispatch(actions.addProductToFreezer(VANILLA, 5));

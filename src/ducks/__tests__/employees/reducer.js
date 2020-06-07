41845da29abc4e0c7212aa45061830f4de5775e4
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-06-07 21:33:07 +0800

    feature: store employee data

---
+++ src/ducks/__tests__/employees/reducer.js
@@ -0,0 +1,59 @@
+import { types, reducer } from "../../employees";
+
+describe("Fetching employees", () => {
+  describe("Request start", () => {
+    it("should set the loading property to true when fetching starts", () => {
+      const result = reducer(undefined, {
+        type: types.FETCH_EMPLOYEES_REQUEST
+      });
+
+      expect(result.loading).toEqual(true);
+    });
+  });
+
+  describe("Successful fetch", () => {
+    it("should set the loading property to false", () => {
+      const result = reducer(
+        { loading: true },
+        {
+          type: types.FETCH_EMPLOYEES_SUCCESS
+        }
+      );
+
+      expect(result.loading).toEqual(false);
+    });
+
+    it("should store the employee data", () => {
+      const fakeData = [1, 2, 3];
+      const result = reducer(undefined, {
+        type: types.FETCH_EMPLOYEES_SUCCESS,
+        payload: fakeData
+      });
+
+      expect(result.data).toEqual(fakeData);
+    });
+  });
+
+  describe("Failed fetch", () => {
+    it("should set the loading property to false", () => {
+      const result = reducer(
+        { loading: true },
+        {
+          type: types.FETCH_EMPLOYEES_FAILURE
+        }
+      );
+
+      expect(result.loading).toEqual(false);
+    });
+
+    it("should store the error", () => {
+      const fakeError = "404 Not Found";
+      const result = reducer(undefined, {
+        type: types.FETCH_EMPLOYEES_FAILURE,
+        payload: fakeError
+      });
+
+      expect(result.error).toEqual(fakeError);
+    });
+  });
+});
--- src/ducks/employees.js
+++ src/ducks/employees.js
@@ -6,10 +6,31 @@ export const types = {
   FETCH_EMPLOYEES_FAILURE: "FETCH_EMPLOYEES_FAILURE"
 };
 
-const DEFAULT_STATE = {};
+const DEFAULT_STATE = {
+  loading: false,
+  data: [],
+  error: null
+};
 
 export function reducer(state = DEFAULT_STATE, action) {
   switch (action.type) {
+    case types.FETCH_EMPLOYEES_REQUEST:
+      return {
+        ...state,
+        loading: true
+      };
+    case types.FETCH_EMPLOYEES_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload
+      };
+    case types.FETCH_EMPLOYEES_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload
+      };
     default:
       return state;
   }

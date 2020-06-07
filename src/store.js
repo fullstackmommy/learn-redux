8820303b5ad6d8d17cc74a64f45b3ff3f9c37e01
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-06-07 15:30:57 +0800

    feature: use redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,11 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import reduxThunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
 import { reducer as freezer } from "./ducks/freezer";
-import logger from "./middleware/logger";
 
 const rootReducer = combineReducers({
   freezer,
   foo: (state = { hello: "world" }, action) => state
 });
 
-export default createStore(rootReducer, applyMiddleware(reduxThunk, logger));
+export default createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxThunk))
+);

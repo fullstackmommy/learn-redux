cd9c8175b8f782811d282abf759e00326de76ee2
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-06-08 23:23:32 +0800

    chore: use react-redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import App from "./components/App/App";
 import * as FLAVORS from "./constants/flavors";
 import { actions } from "./ducks/freezer";
-import store from "./store";
 
 setTimeout(() => {
   store.dispatch(actions.addProductToFreezer(FLAVORS.STRAWBERRY, 5));
 }, 1500);
-render(<App />, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);

c14bd4b2638bdead8769b6830eb0c5b590126463
Nicole Budiman <nicole.budiman@thoughtworks.com>
2020-06-08 22:55:05 +0800

    chore: split freezer container component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Freezer from '../Freezer/Freezer';
-import NewOrder from '../NewOrder/NewOrder';
-import { Column, Row } from '../Grid/Grid';
-import OrderOverview from '../OrderOverview/OrderOverview';
-import Colleagues from '../Colleagues/Colleagues';
+import FreezerContainer from "../Freezer/FreezerContainer";
+import NewOrder from "../NewOrder/NewOrder";
+import { Column, Row } from "../Grid/Grid";
+import OrderOverview from "../OrderOverview/OrderOverview";
+import Colleagues from "../Colleagues/Colleagues";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <main className="app">
-        <img className="logo" src="/logo.svg" alt="Mister Melty's Ice Cream"/>
+        <img className="logo" src="/logo.svg" alt="Mister Melty's Ice Cream" />
         <Row>
           <Column>
-            <Freezer />
+            <FreezerContainer />
           </Column>
           <Column>
             <NewOrder />
--- src/components/Freezer/Freezer.js
+++ src/components/Freezer/Freezer.js
@@ -1,73 +1,22 @@
 import React, { Component } from "react";
 import Panel from "../Panel/Panel";
 import FreezerFlavor from "../FreezerFlavor/FreezerFlavor";
-import store from "../../store";
-import { actions } from "../../ducks/freezer";
 import Button from "../Button/Button";
-import * as FLAVORS from "../../constants/flavors";
 
 class Freezer extends Component {
-  state = {
-    flavors: store.getState().freezer.flavors,
-    temperature: store.getState().freezer.temperature
-  };
-
-  componentDidMount() {
-    this.unsubscribe = store.subscribe(() => {
-      this.setState({
-        flavors: store.getState().freezer.flavors,
-        temperature: store.getState().freezer.temperature
-      });
-    });
-
-    setInterval(() => {
-      const randomTemperature = -Math.round(Math.random() * 10);
-      store.dispatch(actions.updateTemperature(randomTemperature));
-    }, 2000);
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  handleClickRestock = flavorName => {
-    const amount = parseInt(
-      window.prompt(`Enter amount to restock ${flavorName}`)
-    );
-    if (!isNaN(amount)) {
-      store.dispatch(actions.addProductToFreezer(flavorName, amount));
-    }
-  };
-
-  handleClickAddProduct = () => {
-    const allFlavorName = Object.keys(FLAVORS);
-    const flavorName = window.prompt(
-      `Enter flavor name to restock. Choose from ${allFlavorName.join(", ")}`
-    );
-
-    if (FLAVORS[flavorName]) {
-      this.handleClickRestock(flavorName);
-    }
-  };
-
-  handleClickFlavor = flavorName => {
-    store.dispatch(actions.removeScoop(flavorName));
-  };
-
   render() {
-    console.log(this.state.flavors);
-    const flavors = Object.keys(this.state.flavors).map(flavorName => (
+    const flavors = Object.keys(this.props.flavors).map(flavorName => (
       <FreezerFlavor
         key={flavorName}
-        onClickRestock={() => this.handleClickRestock(flavorName)}
-        onClickFlavor={() => this.handleClickFlavor(flavorName)}
+        onClickRestock={() => this.props.onClickRestock(flavorName)}
+        onClickFlavor={() => this.props.onClickFlavor(flavorName)}
         flavorName={flavorName}
-        scoops={this.state.flavors[flavorName]}
+        scoops={this.props.flavors[flavorName]}
       />
     ));
     return (
-      <Panel title={`Freezer (°${this.state.temperature || 0}C)`}>
-        <Button label="Add product" onClick={this.handleClickAddProduct} />
+      <Panel title={`Freezer (°${this.props.temperature || 0}C)`}>
+        <Button label="Add product" onClick={this.props.onClickAddProduct} />
         {flavors}
       </Panel>
     );
